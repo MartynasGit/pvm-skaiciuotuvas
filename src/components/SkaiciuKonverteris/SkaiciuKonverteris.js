c572b74c0b5d079c0ff3f4f21cf6d274e635ce7c
Martynas Gumuliauskas <Mourcius@gmail.com>
2022-08-12 20:43:55 +0300

    Patobilinta lietuviu kalba

--- src/components/SkaiciuKonverteris/SkaiciuKonverteris.js
+++ src/components/SkaiciuKonverteris/SkaiciuKonverteris.js
@@ -5,21 +5,31 @@ import './SkaiciuKonverteris.css';
 const SkaiciuKonverteris = ({num}) => {
   num = parseInt(num)
 
-  const ones = ["", "vienas", "du", "trys", "keturi", "penki", "šesi", "septyni", "aštuoni", "devyni"]
-  const teens  = ["dešimt", "vienuolika", "dvylika", "trylika", "keturiolika", "penkiolika", "šešiolika", "septyniolika", "devyniolika", ]
-  const tens = ["", "", "dvidešimt", "trisdešimt", "keturiasdešimt", "penkiasdešimt", "šešiasdešimt", "septyniasdešimt", "astuoniasdešimt", "devyniasdešimt"]
+  const ones = ["", "vienas", "du", "trys", "keturi", "penki", "šeši", "septyni", "aštuoni", "devyni"]
+  const teens  = ["dešimt", "vienuolika", "dvylika", "trylika", "keturiolika", "penkiolika", "šešiolika", "septyniolika", "aštuoniolika", "devyniolika", ]
+  const tens = ["", "", "dvidešimt", "trisdešimt", "keturiasdešimt", "penkiasdešimt", "šešiasdešimt", "septyniasdešimt", "aštuoniasdešimt", "devyniasdešimt"]
 
   let convert_millions = (num) => {
     if (num >= 1000000) {
-      return convert_millions(Math.floor(num / 1000000)) + " milijonai " + convert_thousands(num % 1000000);
+      return convert_millions(Math.floor(num / 1000000)) + (num >= 1000000 && num < 2000000 ? " milijonas " : " milijonai ") + convert_thousands(num % 1000000);
+    } else {
+      return convert_Tousands(num);
+    }
+  }
+
+  let convert_Tousands = (num) => {
+    if (num >= 10000) {
+      return convert_millions(Math.floor(num / 1000)) + ( " tūkstančių ") + convert_thousands(num % 1000);
     } else {
       return convert_thousands(num);
     }
   }
+
+  // num >= 10000 && num < 999999
   
   function convert_thousands(num) {
     if (num >= 1000) {
-      return convert_hundreds(Math.floor(num / 1000)) + " tukstanciai " + convert_hundreds(num % 1000);
+      return convert_hundreds(Math.floor(num / 1000)) + (num >= 1000 && num <= 1999 ? " tūkstantis " : " tūkstančiai " )+ convert_hundreds(num % 1000);
     } else {
       return convert_hundreds(num);
     }
@@ -27,7 +37,7 @@ const SkaiciuKonverteris = ({num}) => {
   
   function convert_hundreds(num) {
     if (num > 99) {
-      return ones[Math.floor(num / 100)] + " šimtai " + convert_tens(num % 100);
+      return ones[Math.floor(num / 100)] + (num >= 100 && num < 200 ? " šimtas " : " šimtai ") + convert_tens(num % 100);
     } else {
       return convert_tens(num);
     }
@@ -42,7 +52,7 @@ const SkaiciuKonverteris = ({num}) => {
   }
   
   function convert(num) {
-    if (num == 0) return "nulis";
+    if (num === 0) return "nulis";
     else return convert_millions(num);
   }
   
