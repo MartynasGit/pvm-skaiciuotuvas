86fce09724f27d70cbffb75b9a99c6d1aca8ee8e
Martynas Gumuliauskas <Mourcius@gmail.com>
2023-03-29 00:36:57 +0300

    Fixed year

--- src/App.js
+++ src/App.js
@@ -10,10 +10,11 @@ function App() {
   let [pvm, setPvm] =useState(0)
   let [pvmS, setPvmS] = useState(0)
   let [tarifas, setTarifas] = useState(21)
+  const year = new Date().getFullYear();
 
   const numberChange = (target) => {
     
-    if(target.value == ""){
+    if(target.value === ""){
       target.value = 0;
     }
 
@@ -41,10 +42,10 @@ function App() {
 
   const bendraSk = (value) => {
     let cof = 0;
-    if(tarifas == 21){
+    if(tarifas === 21){
       cof = 1.21;
     }
-    else if(tarifas == 9){
+    else if(tarifas === 9){
       cof = 1.09;
     }
     else {
@@ -57,48 +58,69 @@ function App() {
 
   return (
     <div>
-      <div className='pvm-skaiciuokle'>
-        <div className='Header m-4 p-4'>
+      <div className="pvm-skaiciuokle">
+        <div className="Header m-4 p-4">
           <h1>PVM SKAIČIUOKLĖ</h1>
         </div>
-        <div className='Main m-4 p-4'>
-          <div className='box d-flex justify-content-between'>
-            <span className=''>PVM tarifas</span>
-            <div className='col-8 '>
-              <select className='form-control' onChange={(e)=> {selectorOnChange(e.target.value)}}>
+        <div className="Main m-4 p-4">
+          <div className="box d-flex justify-content-between">
+            <span className="">PVM tarifas</span>
+            <div className="col-8 ">
+              <select
+                className="form-control"
+                onChange={(e) => {
+                  selectorOnChange(e.target.value);
+                }}
+              >
                 <option value={21}>21%</option>
                 <option value={9}>9%</option>
                 <option value={5}>5%</option>
               </select>
             </div>
           </div>
-          <div className='d-flex mt-3 justify-content-between'>
-            <span className='me-3 '>Suma (be PVM)</span>
-            <div className='col-8'>
-              <input className='form-control' name="suma" value={+suma.toFixed(2)} onChange={(e) => {numberChange(e.target)}}/>
+          <div className="d-flex mt-3 justify-content-between">
+            <span className="me-3 ">Suma (be PVM)</span>
+            <div className="col-8">
+              <input
+                className="form-control"
+                name="suma"
+                value={+suma.toFixed(2)}
+                onChange={(e) => {
+                  numberChange(e.target);
+                }}
+              />
             </div>
           </div>
-          <div className='d-flex mt-3 justify-content-between'>
-            <span className='me-3 '>PVM suma</span>
-            <div className='col-8'>
-              <input className='form-control' disabled value={+pvm.toFixed(2)}/>
+          <div className="d-flex mt-3 justify-content-between">
+            <span className="me-3 ">PVM suma</span>
+            <div className="col-8">
+              <input
+                className="form-control"
+                disabled
+                value={+pvm.toFixed(2)}
+              />
             </div>
           </div>
-          <div className='d-flex mt-3 justify-content-between'>
-            <span className='me-3 '> Bendra suma (su PVM)</span>
-            <div className='col-8'>
-              <input className='form-control' name="bendra" value={+pvmS.toFixed(2)} onChange={(e) => numberChange(e.target)} />
+          <div className="d-flex mt-3 justify-content-between">
+            <span className="me-3 "> Bendra suma (su PVM)</span>
+            <div className="col-8">
+              <input
+                className="form-control"
+                name="bendra"
+                value={+pvmS.toFixed(2)}
+                onChange={(e) => numberChange(e.target)}
+              />
             </div>
           </div>
         </div>
-        <SkaiciuKonverteris
-        num = {pvmS}
-        />
-        <div className='Bottom m-4'>Įveskite sumą be PVM arba bendrą sumą (su PVM). </div>
+        <SkaiciuKonverteris num={pvmS} />
+        <div className="Bottom m-4">
+          Įveskite sumą be PVM arba bendrą sumą (su PVM).{" "}
+        </div>
       </div>
-      <span class="fw-bold copy fs-6"> 2022 © MartynasGit</span>
+      <span className="fw-bold copy fs-6"> {year} © MartynasGit</span>
     </div>
-  )
+  );
 }
 
 export default App;
